61f957aef3a32f083db5b1fd51b74e3fe97b4c1f
dennis <denhouston1@icloud.com>
2022-02-28 16:44:01 +0000

    added top_up and balance
    Co-authored-by: Abdul Rehman Hussain <arhussain1@users.noreply.github.com>

--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -1,5 +1,19 @@
 require 'oystercard'
 
 describe Oystercard do
-  
+  it 'has a balance of 0 on the card' do
+    expect(subject.balance).to eq(0)
+  end
+  it 'should respond to the top_up method' do
+    expect(subject).to respond_to(:top_up)
+  end
+
+  it 'should return 5 when passed an argument of 5' do
+    expect(subject.top_up(5)).to eq(5)
+  end
+
+  it 'should top up the card balance by 5' do
+    subject.top_up(5)
+    expect(subject.balance).to eq(5)
+  end
 end
