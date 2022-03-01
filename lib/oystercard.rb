822793a8100b0899d037340e3a9261416ec00fbc
dennis <denhouston1@icloud.com>
2022-03-01 17:04:07 +0000

    Added functionality
    Co-authored-by: Abdul Rehman Hussain <arhussain1@users.noreply.github.com>
    
    - added balance boundaries using constants
    - added touch in and touch out methods
    - added raise error for max and min balances
    - can now deduct fare from card balance when touching out

--- lib/oystercard.rb
+++ lib/oystercard.rb
@@ -2,19 +2,47 @@ class Oystercard
   attr_reader :balance
 
   MAX_BALANCE = 90
+  MIN_BALANCE = 1
 
   def initialize
     @balance = 0
+    @being_used = false
   end
 
   def top_up(amount)
-    total_amount = balance + amount
-    raise ("Exceeded max balance") if total_amount > MAX_BALANCE
+    raise ("Exceeded max balance") if exceeded_balance?(amount)
     @balance += amount
   end
 
-  def deduct(amount)
-    @balance -= amount
+  def touch_in
+    raise("You have already touched in") if in_journey?
+    raise("Please top up, not enough money") if balance_too_low?
+    puts "you have now touched in"
+    @being_used = true
+  end
+
+  def touch_out
+    raise("You have not touched in") unless in_journey?
+    deduct && @being_used = false
+  end
+
+  def in_journey?
+    @being_used
+  end
+
+  private
+  def deduct
+    fare = 1
+    @balance -= fare
+  end
+  
+  def balance_too_low?
+    @balance < MIN_BALANCE
+  end
+
+  def exceeded_balance?(amount)
+    total_amount = balance + amount
+    total_amount > MAX_BALANCE
   end
   
 end
--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -1,37 +1,101 @@
 require 'oystercard'
 
 describe Oystercard do
-  it 'has a balance of 0 on the card' do
-    expect(subject.balance).to eq(0)
-  end
-  it 'should respond to the top_up method' do
-    expect(subject).to respond_to(:top_up)
+  describe "testing the :top_up method" do
+    it 'has a balance of 0 on the card' do
+      expect(subject.balance).to eq(0)
+    end
+    it 'should respond to the top_up method' do
+      expect(subject).to respond_to(:top_up)
+    end
+
+    it 'should return 5 when passed an argument of 5' do
+      expect(subject.top_up(5)).to eq(5)
+    end
+
+    it 'should top up the card balance by 5' do
+      subject.top_up(5)
+      expect(subject.balance).to eq(5)
+    end
+
+    it "should raise an error when top_up result in balance above #{Oystercard::MAX_BALANCE}" do
+      expect do
+        subject.top_up(Oystercard::MAX_BALANCE + 5)
+      end.to raise_error("Exceeded max balance")
+    end
+
+    it "should not raise and error when top_up results in less than #{Oystercard::MAX_BALANCE} balance" do
+      expect do
+        subject.top_up(Oystercard::MAX_BALANCE)
+      end.not_to raise_error
+    end
+
   end
 
-  it 'should return 5 when passed an argument of 5' do
-    expect(subject.top_up(5)).to eq(5)
+  describe "testing :touch_in method" do
+    it "should respond to :touch_in" do
+      expect(subject).to respond_to(:touch_in)
+    end
+
+    it "should raise an error if min balance hasn't been met" do
+      expect{ subject.touch_in }.to raise_error("Please top up, not enough money")
+    end
+
+    it "should not raise an error if balance is above minimum balance" do
+      subject.top_up(1)
+      expect{ subject.touch_in }.not_to raise_error
+    end
+
+    it "should not be able to :touch_in if in_journey?" do
+      subject.top_up(5)
+      subject.touch_in
+      expect{ subject.touch_in }.to raise_error("You have already touched in")
+    end
+
   end
 
-  it 'should top up the card balance by 5' do
-    subject.top_up(5)
-    expect(subject.balance).to eq(5)
+  describe "testing :touch_out method" do
+    it "should respond to :touch_out" do
+      expect(subject).to respond_to(:touch_out)
+    end
+
+    it "should deduct the fare of £1 from the balance when calling :touch_out" do
+      subject.top_up(5)
+      subject.touch_in
+      expect{ subject.touch_out }.to change{subject.balance}.by(-1)
+    end
+
+    it "should raise an error if :touch_out is called before :touch_in" do
+      expect{ subject.touch_out }.to raise_error("You have not touched in")
+    end
   end
 
-  it "should raise an error when top_up result in balance above #{Oystercard::MAX_BALANCE}" do
-    expect do
-      subject.top_up(Oystercard::MAX_BALANCE + 5)
-    end.to raise_error("Exceeded max balance")
+  describe "testing the :in_journey? method" do
+    it "should respond to :in_journey?" do
+      expect(subject).to respond_to(:in_journey?)
+    end
+
+    it "should return a boolean value" do
+      expect(subject.in_journey?).to eq(true).or eq(false)
+    end
+
+    it "should not be in a journey when a new card is created" do
+      expect(subject).not_to be_in_journey
+    end
+
+    it "should be in a journey when a card is touched in" do
+      subject.top_up(1)
+      subject.touch_in
+      expect(subject).to be_in_journey
+    end
+
+    it "should revert to not being in a journey once touched out" do
+      subject.top_up(1)
+      subject.touch_in
+      subject.touch_out
+      expect(subject).not_to be_in_journey
+    end
+
   end
 
-  it "should not raise and error when top_up results in less than #{Oystercard::MAX_BALANCE} balance" do
-    expect do
-      subject.top_up(Oystercard::MAX_BALANCE)
-    end.not_to raise_error
-  end
-
-  it 'should decrease the balance by 5 when deduct(5) is implemented' do
-    subject.top_up(10)
-    subject.deduct(5)
-    expect(subject.balance).to eq(5)
-  end
 end
